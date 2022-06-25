fbde13bcddd249e3dd3ed0772d3315aa2c30629c
Reshma Sarasanapalli <reshmasarasanapalli@reshmas-mbp.lan>
2022-06-25 14:51:46 -0400

    ArrayReverse code

--- src/com/ArraysJava/_1_NumberOfPairsInArrayIsEqualToSum.java
+++ src/com/ArraysJava/_1_NumberOfPairsInArrayIsEqualToSum.java
@@ -2,35 +2,35 @@ package com.ArraysJava;
 
 import java.util.Scanner;
 
-public class NumberOfPairsInArrayIsEqualToSum {
+public class _1_NumberOfPairsInArrayIsEqualToSum {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         int n;
-        int sum=4;
+        int sum = 4;
         int[] a = new int[6];
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the number of values in array");
         n = s.nextInt();
-        System.out.println("Please enter the values of the array");
-        for(int i=0;i<n;i++){
-            a[i] = s.nextInt();
 
+        System.out.println("Please enter the values of the array");
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
         }
+
         System.out.println("The elements of teh array are");
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             System.out.println(a[i]);
         }
-        
-        for(int i =0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                if(a[i]+a[j] ==sum){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (a[i] + a[j] == sum) {
                     System.out.println("The values of i and j are equal to sum");
                     System.out.println(a[i]);
                     System.out.println(a[j]);
                     break;
                 }
-
             }
         }
     }
